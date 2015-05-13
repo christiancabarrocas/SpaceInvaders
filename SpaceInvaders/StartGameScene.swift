c8061a2b0e461dee90e6a393661fd1dab6a1a086
Christian Cabarrocas <ccabarrocas@MacBook-Pro-de-Christian.local>
2015-05-13 21:48:04 +0200

    [SI] StartGameScene

---
+++ SpaceInvaders/StartGameScene.swift
@@ -0,0 +1,20 @@
+//
+//  StartGameScene.swift
+//  SpaceInvaders
+//
+//  Created by Christian Cabarrocas on 13/05/15.
+//  Copyright (c) 2015 Wasabilabs. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class StartGameScene: SKScene {
+
+    override func didMoveToView(view: SKView) {
+        let startGameButton = SKSpriteNode(imageNamed: "newgamebtn")
+        startGameButton.position = CGPointMake(size.width/2,size.height/2 - 100)
+        startGameButton.name = "startgame"
+        addChild(startGameButton)
+    }
+}
